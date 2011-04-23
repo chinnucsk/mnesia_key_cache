36996bc3930a6748adb1c161a1d99caf707e5050
Ryosuke Nakai <twisted.mind@voluntas.net>
2011-04-23 22:37:30 +0900

    Add eunit test

--- test/mnesia_key_cache_tests.erl
+++ test/mnesia_key_cache_tests.erl
@@ -8,35 +8,53 @@
                 value0 :: binary(),
                 value1 :: binary()}).
 
-%% activity_test_() ->
-%%   {setup,
-%%     fun() ->
-%%       application:start(mnesia_key_cache)
-%%       mnesia:create_table(?STORE_TABLE,
-%%                           [{frag_properties,
-%%                             [{node_pool, [node()]},
-%%                              {n_fragments, 128}]},
-%%                            {record_name, store},
-%%                            {attributes, record_info(fields, store)}]),
-%%       F = fun(_) ->
-%%             mnesia:activity(async_dirty,
-%%                             fun mnesia:write/3,
-%%                             [?STORE_TABLE,
-%%                              #store{key    = crypto:rand_bytes(32),
-%%                                     value0 = crypto:rand_bytes(32),
-%%                                     value1 = crypto:rand_bytes(32)},
-%%                              sticky_write],
-%%                             mnesia_frag)
-%%           end,
-%%       lists:foreach(F, lists:duplicate(5000, dummy)),
-%%       mnesia_key_cache:start(Table),
-%%       ok
-%%     end,
-%%     fun(_) ->
-%%       application:stop(mnesia_key_cache),
-%%       ok
-%%     end,
-%%     [
-%%       ?_assertEqual(, )
-%%     ]
-%%   }.
+activity_test_() ->
+  {setup,
+    fun() ->
+      mnesia:start(),
+      application:start(mnesia_key_cache),
+      mnesia:create_table(?STORE_TABLE,
+                          [{frag_properties,
+                            [{node_pool, [node()]},
+                             {n_fragments, 128}]},
+                           {record_name, store},
+                           {attributes, record_info(fields, store)}]),
+      F = fun(_) ->
+            mnesia:activity(async_dirty,
+                            fun mnesia:write/3,
+                            [?STORE_TABLE,
+                             #store{key    = crypto:rand_bytes(32),
+                                    value0 = crypto:rand_bytes(32),
+                                    value1 = crypto:rand_bytes(32)},
+                             sticky_write],
+                            mnesia_frag)
+          end,
+      lists:foreach(F, lists:duplicate(500, dummy)),
+      mnesia_key_cache:start(?STORE_TABLE),
+      lists:foreach(F, lists:duplicate(500, dummy)),
+      ok
+    end,
+    fun(_) ->
+      mnesia:stop(),
+      application:stop(mnesia_key_cache),
+      ok
+    end,
+    ?_test(
+      begin
+        F = fun(Key) ->
+              case mnesia:read(?STORE_TABLE, Key, write) of
+                [] ->
+                  {error, retry};
+                [_] ->
+                  mnesia:delete(?STORE_TABLE, Key, write),
+                  ok
+              end
+            end,
+        lists:foreach(fun(_) ->
+                        mnesia_key_cache:activity(?STORE_TABLE, F)
+                      end,
+                      lists:duplicate(1000, dummy)),
+        ?assertEqual({error, not_found}, mnesia_key_cache:activity(?STORE_TABLE, F))
+      end
+    )
+  }.
